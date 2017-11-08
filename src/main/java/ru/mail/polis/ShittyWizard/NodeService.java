894ce174b3f79af02598e4b44fe4edf643efe7b7
Vladislav Anisimov <anisimov.develop@gmail.com>
2017-11-08 21:50:58 +0300

    2nd try

--- src/main/java/ru/mail/polis/ShittyWizard/NodeService.java
+++ src/main/java/ru/mail/polis/ShittyWizard/NodeService.java
@@ -65,7 +65,6 @@ public class NodeService implements KVService {
         try{
             while (true) {
                 server.getAddress();
-                System.out.println("Node still alive.");
             }
         } catch(Exception e){
             e.printStackTrace();
